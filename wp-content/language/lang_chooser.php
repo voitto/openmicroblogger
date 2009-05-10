c60bfd059a05b849717a7b80d4f58ae52c84ebca
Brian <brian@megapump.com>
2009-05-09 18:53:40 -0700

    use default lang

--- wp-content/language/lang_chooser.php
+++ wp-content/language/lang_chooser.php
@@ -10,19 +10,21 @@ else
 $language_file = 'wp-content/language/'.$language_selected.'.php';
 
 if ( file_exists( $language_file )) {
+  
   include $language_file;
+  
 } else {
   
   if (signed_in()) {
     $Setting =& $db->model('Setting');
     $lang = $Setting->find_by(array('name'=>'config.env.lang','profile_id'=>get_profile_id()));
-    
-    $txt = unserialize($lang->data);
+    if ($lang)
+      $txt = unserialize($lang->data);
   }
-
+  
 }
 
-if (!(count($txt) > 0))
+if (!(count($txt) > 10))
   include 'wp-content/language/eng.php';
 
 
