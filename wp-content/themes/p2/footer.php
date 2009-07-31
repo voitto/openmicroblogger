77d2f7ed81446db9bb3f68c123ef6cbeab50741a
Brian <brian@megapump.com>
2009-07-31 01:55:43 -0700

    twivatar

--- wp-content/themes/p2/footer.php
+++ wp-content/themes/p2/footer.php
@@ -2,7 +2,10 @@
 </div> <!-- // wrapper -->
 
 	<div id="footer">
-		<p><a href="http://openmicroblogger.org/">OpenMicroblogger <?php global $ombversion; echo $ombversion; ?></a> | P2 theme by <a href="http://automattic.com/">Automattic</a><?php render_partial('pagespan'); ?></p>
+		<p><a href="http://openmicroblogger.org/">OpenMicroblogger <?php global $ombversion; echo $ombversion; ?></a> | P2 theme by <a href="http://automattic.com/">Automattic</a><?php render_partial( array(
+    'resource'=>'posts',
+    'action'=>'pagespan'
+  )); ?></p>
 	</div>
 	
 <?php wp_footer(); ?>
