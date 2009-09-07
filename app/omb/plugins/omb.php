0acc7c032d4fa679c04aa6ab51455d79c8b36771
Brian Hendrickson <brian@megapump.com>
2009-09-06 17:00:16 -0700

    status update fix

--- app/omb/plugins/omb.php
+++ app/omb/plugins/omb.php
@@ -374,8 +374,10 @@ function wp_set_post_fields_after( &$model, &$rec ) {
       $rec->set_value( 'parent_id',$_POST['parent_id']);
       $rec->set_value( 'local',1);
     } else {
-      $rec->set_value( 'parent_id',0);
-      $rec->set_value( 'local',0);
+      if (!isset($rec->attributes['parent_id']))
+        $rec->set_value( 'parent_id',0);
+      if (!isset($rec->attributes['local']))
+        $rec->set_value( 'local',0);
     }
     $rec->save_changes();
   }
